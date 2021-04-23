04fbb74d44b046742b534f95947eb094fb046e39
Felipe Affonso <felipe.affonso@mths.ca>
2021-04-23 14:22:52 +0000

    commit message

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Created by: Felipe Garcia Affonso
+# Created on: April 2021
+# This is the "Hello, World!" program, with proper style
+
+def main():
+    print('Hello, World!')
+
+
+if __name__ == "__main__":
+    main()
